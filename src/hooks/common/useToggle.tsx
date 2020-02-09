c2d134575e59c82e39044388b741acf1e8f28449
Tornike Menabde <tokamenabde@gmail.com>
2020-02-09 21:40:51 +0400

    useCallbeck in toglle hook fix inifinte render

--- src/hooks/common/useToggle.tsx
+++ src/hooks/common/useToggle.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export const useToggle = (defaultValue: boolean = false) => {
   const [isActive, setToggle] = useState(defaultValue);
 
-  const toggle = () => {
+  const toggle = useCallback(() => {
     setToggle(prevState => !prevState);
-  };
+  }, [setToggle]);
 
-  const setActive = () => {
+  const setActive = useCallback(() => {
     setToggle(true);
-  };
+  }, [setToggle]);
 
-  const setInActive = () => {
+  const setInActive = useCallback(() => {
     setToggle(false);
-  };
+  }, [setToggle]);
 
   return { isActive, toggle, setActive, setInActive };
 };
